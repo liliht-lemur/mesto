eb748adb158d899adcc9c15282eea3ad7288374a
Liliht <liliht@MacBook-Pro-Liliht.local>
2023-03-14 22:43:21 +0400

    Added animation

--- js/script.js
+++ js/script.js
@@ -24,10 +24,6 @@ const initialCards = [
   {
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-  {
-    name: 'PPP',
-    link: './images/cards/kamchatka.jpg'
   }
 ];
 const elements = document.querySelector('.elements');
