b8ea67032e96b364225e805941b1ea885669f72d
Liliht <liliht@MacBook-Pro-Liliht.local>
2023-03-24 23:48:14 +0400

    Changed adaptation of modal windows

--- js/script.js
+++ js/script.js
@@ -37,6 +37,8 @@ const inputNameFormProfile = modalWindowProfile.querySelector('.modal__descripti
 const inputAboutSelfFormProfile = modalWindowProfile.querySelector('.modal__description_type_about-self');
 const formProfile = modalWindowProfile.querySelector('.modal__edit');
 
+
+
 const profile = document.querySelector('.profile');
 const myName =  profile.querySelector('.profile__title');
 const aboutSelf = profile.querySelector('.profile__subtitle');
@@ -51,6 +53,7 @@ const modalCloseImg = modalWindowImg.querySelector('.modal__close_img');
 
 editButton.addEventListener('click', function(){
   toggleVisibilityModalWindow(modalWindowProfile);
+
   inputNameFormProfile.value = myName.textContent;
   inputAboutSelfFormProfile.value = aboutSelf.textContent;
 });
@@ -63,6 +66,11 @@ formProfile.addEventListener('submit', handleFormProfileSubmit);
 
 const modalWindowAddNewCard = document.querySelector('.modal__overlay_add');
 const buttonCloseModalWindowAddNewCard = modalWindowAddNewCard.querySelector('.modal__close_add');
+
+
+
+const aaa = document.getElementById('heading-input');
+aaa.value='a'
 
 addClickEvent(addButton, modalWindowAddNewCard);
 addClickEvent(buttonCloseModalWindowAddNewCard, modalWindowAddNewCard, false);
@@ -80,8 +88,90 @@ const imagePhoto = document.querySelector('.modal__photo');
 
 
 
+// block with validation
 
-// block with functions
+
+const showInputError = (formElem, inputElem, errorMessage) => {
+  const errorElement = formElem.querySelector(`.${inputElem.id}-error`);
+
+  inputElem.classList.add('modal__description_type_error');
+  errorElement.classList.add('modal__description-error_active');
+  errorElement.textContent = errorMessage;
+}
+
+const hideInputError = (formElem, inputElem) => {
+  const errorElement = formElem.querySelector(`.${inputElem.id}-error`);
+
+  inputElem.classList.remove('modal__description_type_error');
+  errorElement.classList.remove('modal__description-error_active');
+  errorElement.textContent = '';
+}
+
+const isValid = (formElem, inputElem) => {
+  if (!inputElem.validity.valid) {
+    showInputError(formElem, inputElem, inputElem.validationMessage);
+  } else {
+    hideInputError(formElem, inputElem);
+  }
+}
+
+const setEventListeners = (formElem) => {
+  const inputList = Array.from(formElem.querySelectorAll('.modal__description'));
+  
+  inputList.forEach(inputElem => inputElem.addEventListener('input', (event)=> {
+    isValid (formElem, inputElem);
+  })); 
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.forms'));
+
+  formList.forEach((formElem) => {
+    // formElem.addEventListener("submit", function (evt) {
+    //   evt.preventDefault();
+    // });
+
+    setEventListeners(formElem)
+  });
+}
+
+// const enableValidation = () => {
+//   const formList = Array.from(document.querySelectorAll('.forms'));
+
+//   formList.forEach((formElem) => {
+//     formElem.addEventListener('submit', function (evt) {
+//       evt.preventDefault();
+//     });
+
+//     const fieldsetList = Array.from(formElem.querySelectorAll('.form__set'));
+    
+//     fieldsetList.forEach((fieldSet) => {
+//        setEventListeners(fieldSet);
+//      });
+//   });  
+// };
+
+enableValidation();
+
+
+// block 
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}; 
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if(hasInvalidInput(inputList)){
+    buttonElement.classList.add('button_inactive');
+  }else {
+    buttonElement.classList.remove('button_inactive');
+  }
+}; 
+
+
+// block with old functions
 
 
 function toggleVisibilityModalWindow(modalWindow, isNeedOpen = true ){
@@ -91,7 +181,7 @@ function toggleVisibilityModalWindow(modalWindow, isNeedOpen = true ){
 function addClickEvent(target, item, isAddEvent = true) {
   target.addEventListener('click', function(){
     toggleVisibilityModalWindow(item, isAddEvent);
-  })
+  });
 }
 
 
